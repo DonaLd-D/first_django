1c0ea4bd66bb14e4a8337badd82671e94709162f
DonaLd-D <57483340+DonaLd-D@users.noreply.github.com>
2021-01-10 19:13:02 +0800

    py 模板语法

--- HelloWorld/urls.py
+++ HelloWorld/urls.py
@@ -3,5 +3,19 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('runoob/', views.runoob),
+    path('demo01/', views.demo01),
+    path('demo02/', views.demo02),
+    path('demo03/', views.demo03),
+    path('demo04/', views.demo04),
+    path('demo05/', views.demo05),
+    path('demo06/', views.demo06),
+    path('demo07/', views.demo07),
+    path('demo08/', views.demo08),
+    path('demo09/', views.demo09),
+    path('demo10/', views.demo10),
+    path('demo11/', views.demo11),
+    path('demo12/', views.demo12),
+    path('demo13/', views.demo13),
+    path('demo14/', views.demo14),
+    path('demo15/', views.demo15),
 ]
--- HelloWorld/views.py
+++ HelloWorld/views.py
@@ -1,6 +1,78 @@
 from django.shortcuts import render
 
 
-def runoob(request):
+def demo01(request):
+    context = {}
+    context['hello'] = 'Hello World!'
+    return render(request, 'demo01.html', context)
+
+
+def demo02(request):
+    views_name = "菜鸟教程"
+    return render(request, "demo02.html", {"name": views_name})
+
+
+def demo03(request):
+    views_list = ["菜鸟教程1", "菜鸟教程2", "菜鸟教程3"]
+    return render(request, "demo03.html", {"views_list": views_list})
+
+
+def demo04(request):
+    views_dict = {"name": "菜鸟教程"}
+    return render(request, "demo04.html", {"views_dict": views_dict})
+
+
+def demo05(request):
     name = 0
-    return render(request, "runoob.html", {"name": name})
+    return render(request, "demo05.html", {"name": name})
+
+
+def demo06(request):
+    name = "菜鸟教程"
+    return render(request, "demo06.html", {"name": name})
+
+
+def demo07(request):
+    num = 1024
+    return render(request, "demo07.html", {"num": num})
+
+
+def demo08(request):
+    import datetime
+    now = datetime.datetime.now()
+    return render(request, "demo08.html", {"time": now})
+
+
+def demo09(request):
+    views_str = "菜鸟教程"
+    return render(request, "demo09.html", {"views_str": views_str})
+
+
+def demo10(request):
+    views_str = "<a href='https://www.runoob.com/' target='_blank'> 点击跳转</a>"
+    return render(request, "demo10.html", {"views_str": views_str})
+
+
+def demo11(request):
+    views_num = 88
+    return render(request, "demo11.html", {"num": views_num})
+
+
+def demo12(request):
+    views_list = ["菜鸟教程", "菜鸟教程1", "菜鸟教程2", "菜鸟教程3"]
+    return render(request, "demo12.html", {"views_list": views_list})
+
+
+def demo13(request):
+    views_dict = {"name": "菜鸟教程", "age": 18}
+    return render(request, "demo13.html", {"views_dict": views_dict})
+
+
+def demo14(request):
+    views_list = ["a", "b", "c", "d", "e"]
+    return render(request, "demo14.html", {"listvar": views_list})
+
+
+def demo15(request):
+    views_list = []
+    return render(request, "demo15.html", {"listvar": views_list})
